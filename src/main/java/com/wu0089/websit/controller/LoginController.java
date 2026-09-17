114ff2a68fd392b93739d513765b24727b134f66
lcgpc <leechengpc@gmail.com>
2026-09-17 17:53:39 +0800

    login

--- src/main/java/com/wu0089/websit/controller/LoginController.java
+++ src/main/java/com/wu0089/websit/controller/LoginController.java
@@ -3,12 +3,12 @@ package com.wu0089.websit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller 
-public class ProductsController {
 
-    @GetMapping ("/products")
-    public String products(){
-        return "products";
+@Controller 
+public class LoginController {
+    @GetMapping("login")
+    public String login() {
+        return "login";
     }
     
 }
