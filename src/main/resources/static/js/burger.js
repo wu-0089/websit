bbffd9c00379b345bc13ab7656db9f3a65aa4cc2
lcgpc <leechengpc@gmail.com>
2026-09-17 16:36:20 +0800

    list add

--- src/main/resources/static/js/burger.js
+++ src/main/resources/static/js/burger.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuNButton = document.querySelector(".menu-button");
-    const mainuNav     = document.querySelector(".main-nav");
+    const mainNav     = document.querySelector(".main-nav");
 
-    if(!menuNButton || !mainuNav){
+    if(!menuNButton || !mainNav){
         return;
     }
     //開關
     menuNButton.addEventListener("click", function(){
-        const isopen = mainuNav.classList.toggle("active")
+        const isopen = mainNav.classList.toggle("active")
         menuNButton.textContent = isopen ? "X" : "三";
    
             }
